0ef16790b564a04912c273580b942fd0642220e7
jkim971201 <jkim97@postech.ac.kr>
2024-05-24 00:47:23 +0900

    dbInst:

--- src/database/db/dbInst.cpp
+++ src/database/db/dbInst.cpp
@@ -29,34 +29,66 @@ dbInst::setLocation(int placementX, int placementY)
 	{
 		case Orient::N :
 		{
+      dx_ = macro_->sizeX();
+      dy_ = macro_->sizeY();
+			lx_ = placementX;
+			ly_ = placementY;
 			break;
 		}
 		case Orient::S :
 		{
+      dx_ = macro_->sizeX();
+      dy_ = macro_->sizeY();
+			lx_ = placementX - dx_;
+			ly_ = placementY - dy_;
 			break;
 		}
 		case Orient::W :
 		{
+      dx_ = macro_->sizeY();
+      dy_ = macro_->sizeX();
+			lx_ = placementX - dx_;
+			ly_ = placementY;
 			break;
 		}
 		case Orient::E :
 		{
+      dx_ = macro_->sizeY();
+      dy_ = macro_->sizeX();
+			lx_ = placementX;
+			ly_ = placementY - dy_;
 			break;
 		}
 		case Orient::FN :
 		{
+      dx_ = macro_->sizeX();
+      dy_ = macro_->sizeY();
+			lx_ = placementX - dx_;
+			ly_ = placementY;
 			break;
 		}
 		case Orient::FS :
 		{
+      dx_ = macro_->sizeX();
+      dy_ = macro_->sizeY();
+			lx_ = placementX;
+			ly_ = placementY - dy_;
 			break;
 		}
 		case Orient::FW :
 		{
+      dx_ = macro_->sizeY();
+      dy_ = macro_->sizeX();
+			lx_ = placementX;
+			ly_ = placementY;
 			break;
 		}
 		case Orient::FE :
 		{
+      dx_ = macro_->sizeY();
+      dy_ = macro_->sizeX();
+			lx_ = placementX - dx_;
+			ly_ = placementY - dy_;
 			break;
 		}
 	}
@@ -68,6 +100,8 @@ dbInst::print() const
   std::cout << std::endl;
   std::cout << "MACRO : " << macro_->name() << std::endl;
   std::cout << "NAME  : " << name_ << std::endl;
+	std::cout << "LX LY : " << lx() << " " << ly() << std::endl;
+	std::cout << "UX UY : " << ux() << " " << uy() << std::endl;
   std::cout << std::endl;
 }
 
